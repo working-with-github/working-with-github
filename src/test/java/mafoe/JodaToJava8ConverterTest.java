105a1b1a93a5b79297d3de0db8c6ff811840dbb9
martinfoersterling <martinleanderfoersterling@gmail.com>
2018-01-30 11:46:28 +0100

    add test

--- src/test/java/mafoe/JodaToJava8ConverterTest.java
+++ src/test/java/mafoe/JodaToJava8ConverterTest.java
@@ -1,6 +1,7 @@
 package mafoe;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
 import java.time.ZonedDateTime;
@@ -18,5 +19,13 @@ public class JodaToJava8ConverterTest {
         DateTime jodaTime = DateTime.parse("2010-06-30T01:20+02:00");
         ZonedDateTime zonedDateTime = JodaToJava8Converter.convertTimestamp(jodaTime);
         assertEquals("2010-06-30T01:20+02:00", zonedDateTime.toString());
+    }
+
+    @Test
+    public void testConvertLocalDate() {
+
+        LocalDate jodaLocalDate = LocalDate.parse("2010-06-30");
+        java.time.LocalDate localDate = JodaToJava8Converter.convertLocalDate(jodaLocalDate);
+        assertEquals("2010-06-30", localDate.toString());
     }
 }
